dea72e25159903c718620c8314f9fbcfe36b852c
Nadiia2 <studenikina.nadya@gmail.com>
2024-04-09 17:53:34 +0200

    Homework 8

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.item');
-console.log('Number of categories:', items.length);
-
-items.forEach(item => {
-  const headlines = item.querySelector('h2');
-  const items2 = item.querySelectorAll('li');
-  console.log('Category:', headlines.textContent);
-  console.log('Elements:', items2.length);
-});
